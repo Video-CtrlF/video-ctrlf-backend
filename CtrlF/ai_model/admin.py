d4a64fca437bdcda9613c4934d0bba63159cfc89
Jihwan98 <zxwlghksv@gmail.com>
2023-05-28 21:45:37 +0900

    feat: create models.py

--- CtrlF/ai_model/admin.py
+++ CtrlF/ai_model/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.YouTubeURL)
+admin.site.register(models.YouTubeCaption)
+admin.site.register(models.OCRResult)
+admin.site.register(models.STTResult)
