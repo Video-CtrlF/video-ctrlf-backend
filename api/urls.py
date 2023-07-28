87a4c7c1c87d74213e01329b4783a8f77c59fcc8
jihwan98 <jihwan98.dev@gmail.com>
2023-07-28 21:31:55 +0900

    feat: double check & HTTP Method 및 API URL 변경

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("url-check/", views.url_check),
-    path("caption/", views.get_caption),
+    path("check-existence/", views.url_check), 
+    path("captions/", views.get_caption), 
     path("status/", views.get_status),
     path("ai/", views.ai_inference),
     path("ai/result/", views.ai_result),
--- api/views.py
+++ api/views.py
@@ -7,36 +7,48 @@ from rest_framework.decorators import api_view
 from .serializers import *
 
 import threading
+from pytube import YouTube
 
 # Create your views here.
 
-# api/url-check/
-@api_view(['POST'])
+# api/check-existence?url=https://www.youtube.com/~~~/
+@api_view(['GET'])
 def url_check(request):
     # db에 url 이 있는지 확인
-    if request.method == 'POST':
-        url = request.data.get('url')
-        if models.YouTubeURL.objects.filter(url=url).exists(): # DB에 해당 URL이 존재하는지 확인
-            return Response({"isExist" : True})
-        return Response({"isExist" : False})
+    if request.method == 'GET':
+        try:
+            url = request.GET.get('url', None) # Query param
+            if url:
+                url = get_watch_url(url)
+                if models.YouTubeURL.objects.filter(url=url).exists(): # DB에 해당 URL이 존재하는지 확인
+                    return Response({"isExist" : True})
+                return Response({"isExist" : False})
+            else:
+                return Response({"Error" : "url was not sent"})
+        except Exception as e:
+            return Response({"Error" : e})
 
-# api/caption/
+# api/captions/
 @api_view(['POST'])
 def get_caption(request):
     # youtube에서 제공하는 caption을 return
     if request.method == 'POST':
         try:
             url = request.data.get('url')
-            if not models.YouTubeURL.objects.filter(url=url).exists(): # DB에 해당 URL이 없으면
-                # 기본 처리 (db에 url 저장 및 caption)
-                youtubeDefault(url=url)
-            yt_url = models.YouTubeURL.objects.get(url=url)
-            YTCaption_serializer = YTCaptionSerializer(models.YouTubeCaption.objects.filter(url_id=yt_url), many=True)
-            return Response({
-                    "info" : YTInfoSerializer(models.YouTubeInfo.objects.get(url_id=yt_url)).data,
-                    "keywords" : CaptionKeywordSerializer(models.CaptionKeyword.objects.get(url_id=yt_url)).data['keywords']['topN'],
-                    "caption" : YTCaption_serializer.data
-                })
+            if url:
+                url = get_watch_url(url)
+                if not models.YouTubeURL.objects.filter(url=url).exists(): # DB에 해당 URL이 없으면
+                    # 기본 처리 (db에 url 저장 및 caption)
+                    youtubeDefault(url=url)
+                yt_url = models.YouTubeURL.objects.get(url=url)
+                YTCaption_serializer = YTCaptionSerializer(models.YouTubeCaption.objects.filter(url_id=yt_url), many=True)
+                return Response({
+                        "info" : YTInfoSerializer(models.YouTubeInfo.objects.get(url_id=yt_url)).data,
+                        "keywords" : CaptionKeywordSerializer(models.CaptionKeyword.objects.get(url_id=yt_url)).data['keywords']['topN'],
+                        "caption" : YTCaption_serializer.data
+                    })
+            else:
+                return Response({"Error" : "url was not sent"})
         except Exception as e:
             return Response({"Error" : e})
     
@@ -45,57 +57,101 @@ def get_caption(request):
 def ai_inference(request):
     # AI 모델 스레드로 돌리고 status 반환
     if request.method == 'POST':
-        url = request.data.get('url')
         try:
-            ai_obj = AiModel(url)
-            yt_url = models.YouTubeURL.objects.get(url=url)
+            url = request.data.get('url')
+            if url:
+                url = get_watch_url(url)
+                if not models.YouTubeURL.objects.filter(url=url).exists(): # DB에 해당 URL이 없으면
+                    return Response({"Error" : "url not exists"})
 
-            # 스레드로 실행
-            t = threading.Thread(target=inference, args=(yt_url, ai_obj, ))
-            t.daemon = True
-            t.start()
+                yt_url = models.YouTubeURL.objects.get(url=url)
+                if yt_url.status == "success":
+                    return Response({"status" : "success"})
+                elif yt_url.status == "process":
+                    return Response({"status" : "process"})
+                elif yt_url == "fail":
+                    # api/ai/result/ ['DELETE']와 동일한 코드
+                    yt_url = models.YouTubeURL.objects.get(url=url)
+                    ocrResult = models.OCRResult.objects.filter(url_id=yt_url)
+                    ocrResult.delete()
+                    ocrKeyword = models.OCRKeyword.objects.filter(url_id=yt_url)
+                    ocrKeyword.delete()
 
-            # 상태 "process"로 변경
-            yt_url.status = "process"
-            yt_url.save()
-        
-            return Response({"status" : yt_url.status}) # db에서 가져오는 값은 아님. 사실상 "process"가 전달되는 것
+                    sttResult = models.STTResult.objects.filter(url_id=yt_url)
+                    sttResult.delete()
+                    sttKeyword = models.STTKeyword.objects.filter(url_id=yt_url)
+                    sttKeyword.delete()
+
+                ai_obj = AiModel(url)
+
+                # 스레드로 실행
+                t = threading.Thread(target=inference, args=(yt_url, ai_obj, ))
+                t.daemon = True
+                t.start()
+
+                # 상태 "process"로 변경
+                yt_url.status = "process"
+                yt_url.save()
             
+                return Response({"status" : yt_url.status}) # db에서 가져오는 값은 아님. 사실상 "process"가 전달되는 것
+            else:
+                return Response({"Error" : "url was not sent"})
         except Exception as e:
             return Response({"Error" : e})
 
-# api/status/
-@api_view(['POST'])
+# api/statusurl=https://www.youtube.com/~~~/
+@api_view(['GET'])
 def get_status(request):
     # status 반환
-    if request.method == 'POST':
-        url = request.data.get('url')
-        if models.YouTubeURL.objects.filter(url=url).exists(): # DB에 해당 URL이 존재하면
-            yt_url = models.YouTubeURL.objects.get(url=url)
-            return Response({"status" : yt_url.status})
-        else:
-            return Response({"status" : "not exists"})
+    if request.method == 'GET':
+        try:
+            url = request.GET.get('url', None) # Query param
+            if url:
+                url = get_watch_url(url)
+                if models.YouTubeURL.objects.filter(url=url).exists(): # DB에 해당 URL이 존재하면
+                    yt_url = models.YouTubeURL.objects.get(url=url)
+                    return Response({"status" : yt_url.status})
+                else:
+                    return Response({"status" : "url not exists"})
+            else:
+                return Response({"Error" : "url was not sent"})
+        except Exception as e:
+            return Response({"Error" : e})
 
 # api/ai/result/
-@api_view(['POST', 'DELETE'])
+@api_view(['GET', 'DELETE'])
 def ai_result(request):
     # AI Inference 결과 반환
-    if request.method == 'POST':
-        url = request.data.get('url')
+    if request.method == 'GET':
         try:
-            yt_url = models.YouTubeURL.objects.get(url=url)
+            url = request.GET.get('url', None) # Query param
+            if url:
+                url = get_watch_url(url)
+                yt_url = models.YouTubeURL.objects.get(url=url)
 
-            STTserializer = STTResultSerializer(models.STTResult.objects.filter(url_id=yt_url), many=True)
-            OCRserializer = OCRResultSerializer(models.OCRResult.objects.filter(url_id=yt_url), many=True)
-            return Response({
-                "info" : YTInfoSerializer(models.YouTubeInfo.objects.get(url_id=yt_url)).data,
-                "STTkeywords" : STTKeywordSerializer(models.STTKeyword.objects.get(url_id=yt_url)).data['keywords']['topN'],
-                "OCRkeywords" : OCRKeywordSerializer(models.OCRKeyword.objects.get(url_id=yt_url)).data['keywords']['topN'],
-                "scripts" : {
-                    "STT" : STTserializer.data, 
-                    "OCR" : OCRserializer.data
-                }
-            })
+                STTserializer = STTResultSerializer(models.STTResult.objects.filter(url_id=yt_url), many=True)
+                OCRserializer = OCRResultSerializer(models.OCRResult.objects.filter(url_id=yt_url), many=True)
+                sttKeyword = models.STTKeyword.objects.filter(url_id=yt_url)
+                if sttKeyword:
+                    retSttKeyword = STTserializer(sttKeyword).data['keywords']['topN'],
+                else:
+                    retSttKeyword = None
+                ocrKeyword = models.OCRKeyword.objects.filter(url_id=yt_url)
+                if ocrKeyword:
+                    retOcrKeyword = OCRserializer(ocrKeyword).data['keywords']['topN'],
+                else:
+                    retOcrKeyword = None
+                return Response({
+                    "info" : YTInfoSerializer(models.YouTubeInfo.objects.get(url_id=yt_url)).data,
+                    "STTkeywords" : retSttKeyword,
+                    "OCRkeywords" : retOcrKeyword,
+                    "scripts" : {
+                        "STT" : STTserializer.data, 
+                        "OCR" : OCRserializer.data
+                    }
+                })
+            else:
+                return Response({"Error" : "url was not sent"})
         except Exception as e:
             return Response({"Error" : e})
     if request.method == 'DELETE':
@@ -179,3 +235,6 @@ def inference(yt_url, ai_obj):
             print("[AI INFERENCE Error] : ", e)
             yt_url.status = "fail"
             yt_url.save()
+
+def get_watch_url(url):
+    return YouTube(url).watch_url
